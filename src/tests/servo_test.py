10d7e8e7f80cb2146360064d6068115145dc10d0
Shaanukstar123 <shaanukag0@gmail.com>
2023-02-02 00:27:03 +0000

    sensors calibrated

---
+++ src/tests/servo_test.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+import time
+
+# Initialize the GPIO pin as an output
+servo_pin = 25
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+# Create a PWM instance with a 50Hz frequency
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(0)
+
+# Define the duty cycle required to rotate the servo to a specific angle
+def set_angle(angle):
+    duty = (angle / 18) + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.3)
+    GPIO.output(servo_pin, False)
+    #pwm.ChangeDutyCycle(0)
+
+# Example: rotate the servo to 90 degrees
+set_angle(0)
+while True:
+	None
+
+# Clean up the GPIO when done
+GPIO.cleanup()
