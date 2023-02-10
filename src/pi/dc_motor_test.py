1e391f8c8c7ce01dbd34cf1c280153969f622008
Shaanuka Gunaratne <55187271+Shaanukstar123@users.noreply.github.com>
2023-02-10 21:33:45 +0000

    Everything's working together

--- src/pi/dc_motor_test.py
+++ src/pi/dc_motor_test.py
@@ -1,53 +1,70 @@
-from gpiozero import PWMOutputDevice
-from gpiozero import DigitalOutputDevice
+
+import servo_test as servo
+import weight_test as weight
+import RPi.GPIO as GPIO
 from time import sleep
- 
-#///////////////// Define Motor Driver GPIO Pins /////////////////
-# Motor A, Left Side GPIO CONSTANTS
-PWM_DRIVE_LEFT = 21		# ENA - H-Bridge enable pin
-FORWARD_LEFT_PIN = 26	# IN1 - Forward Drive
-REVERSE_LEFT_PIN = 19	# IN2 - Reverse Drive
-# Motor B, Right Side GPIO CONSTANTS
-PWM_DRIVE_RIGHT = 5		# ENB - H-Bridge enable pin
-FORWARD_RIGHT_PIN = 13	# IN1 - Forward Drive
-REVERSE_RIGHT_PIN = 6	# IN2 - Reverse Drive
- 
-# Initialise objects for H-Bridge GPIO PWM pins
-# Set initial duty cycle to 0 and frequency to 1000
-driveLeft = PWMOutputDevice(PWM_DRIVE_LEFT, True, 0, 1000)
-driveRight = PWMOutputDevice(PWM_DRIVE_RIGHT, True, 0, 1000)
- 
-# Initialise objects for H-Bridge digital GPIO pins
-forwardLeft = DigitalOutputDevice(FORWARD_LEFT_PIN)
-reverseLeft = DigitalOutputDevice(REVERSE_LEFT_PIN)
-forwardRight = DigitalOutputDevice(FORWARD_RIGHT_PIN)
-reverseRight = DigitalOutputDevice(REVERSE_RIGHT_PIN)
- 
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pins used by the TB6612FNG controller
+AIN1 = 26
+AIN2 = 19
+STBY = 21
+PWM_PIN = 12
+
+# Set the GPIO pins as outputs
+GPIO.setup(AIN1, GPIO.OUT)
+GPIO.setup(AIN2, GPIO.OUT)
+GPIO.setup(STBY, GPIO.OUT)
+GPIO.setup(PWM_PIN, GPIO.OUT)
+
+# Set the STBY pin to active
+GPIO.output(STBY, GPIO.HIGH)
+
+# Initialize PWM on the PWM_PIN with a frequency of 50 Hz
+pwm = GPIO.PWM(PWM_PIN, 50)
+
 def allStop():
-	forwardLeft.value = False
-	reverseLeft.value = False
-	forwardRight.value = False
-	reverseRight.value = False
-	driveLeft.value = 0
-	driveRight.value = 0
- 
-def forwardDrive():
-	forwardLeft.value = True
-	reverseLeft.value = False
-	forwardRight.value = True
-	reverseRight.value = False
-	driveLeft.value = 1.0
-	driveRight.value = 1.0
- 
-def reverseDrive():
-	forwardLeft.value = False
-	reverseLeft.value = True
-	forwardRight.value = False
-	reverseRight.value = True
-	driveLeft.value = 1.0
-	driveRight.value = 1.0
-	
+    # Set all pins to LOW
+    GPIO.output(AIN1, GPIO.LOW)
+    GPIO.output(AIN2, GPIO.LOW)
+    pwm.stop()
+
+def forwardDrive(speed):
+    # Set the AIN1 pin to HIGH and AIN2 to LOW
+    GPIO.output(AIN1, GPIO.HIGH)
+    GPIO.output(AIN2, GPIO.LOW)
+    # Start PWM with the specified duty cycle
+    pwm.start(speed)
+
+def reverseDrive(speed):
+    # Set the AIN1 pin to LOW and AIN2 to HIGH
+    GPIO.output(AIN1, GPIO.LOW)
+    GPIO.output(AIN2, GPIO.HIGH)
+    # Start PWM with the specified duty cycle
+    pwm.start(speed)
+
+# Example usage
 allStop()
-forwardDrive()
-sleep(5)
+
+servo_pin = 25
+hx = weight.setup_weight()
+pwm_servo = servo.setup_servo()
+
+while True:
+	forwardDrive(20)
+	val = weight.measure(hx)
+	if val>15:
+		allStop()
+		servo.dispense(pwm_servo)
+
+
+sleep(100)
+#allStop()
+#reverseDrive(50)
+#sleep(2)
 allStop()
+
+# Clean up the GPIOs
+GPIO.cleanup()
