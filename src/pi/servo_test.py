35118cd975ea5650c25cfad87bb6ea44186b000f
Shaanuka Gunaratne <55187271+Shaanukstar123@users.noreply.github.com>
2023-02-10 22:18:40 +0000

    update

--- src/pi/servo_test.py
+++ src/pi/servo_test.py
@@ -1,29 +1,42 @@
 import RPi.GPIO as GPIO
+from gpiozero import Servo
 import time
 
-# Initialize the GPIO pin as an output
-servo_pin = 25
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servo_pin, GPIO.OUT)
 
-# Create a PWM instance with a 50Hz frequency
-pwm = GPIO.PWM(servo_pin, 50)
-pwm.start(0)
+# Initialize the GPIO pin as an output
+def setup_servo():
+    servo_pin = 25
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servo_pin, GPIO.OUT)
+
+    # Create a PWM instance with a 50Hz frequency
+    pwm_servo = GPIO.PWM(servo_pin, 50)
+    pwm_servo.start(0)
+    return pwm_servo
 
 # Define the duty cycle required to rotate the servo to a specific angle
-def set_angle(angle):
+def set_angle(angle,pwm_servo):
+    servo_pin = 25
     duty = (angle / 18) + 2
     GPIO.output(servo_pin, True)
-    pwm.ChangeDutyCycle(duty)
+    pwm_servo.ChangeDutyCycle(duty)
     time.sleep(0.3)
     GPIO.output(servo_pin, False)
-    #pwm.ChangeDutyCycle(0)
+    pwm_servo.ChangeDutyCycle(0)
 
-# Example: rotate the servo to 90 degrees
-set_angle(50)
-time.sleep(1)
-set_angle(0)
+def dispense(pwm_servo):
+    target = 50
+    for i in range(0,target,10):
+        set_angle(i,pwm_servo)
+
+    time.sleep(1)
+    set_angle(0,pwm_servo)
 
 
-# Clean up the GPIO when done
-GPIO.cleanup()
+# Servo(50)
+# servo_pin = 25
+# pwm_servo = setup_servo()
+# dispense(pwm_servo)
+
+# # Clean up the GPIO when done
+# GPIO.cleanup()
