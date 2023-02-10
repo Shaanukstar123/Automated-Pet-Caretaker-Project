63b5ac7115ecc53d8dbaf05413a479b3d59b742a
Shaanuka Gunaratne <55187271+Shaanukstar123@users.noreply.github.com>
2023-02-10 12:59:47 +0000

    testCodeMain

--- src/pi/main.py
+++ src/pi/main.py
@@ -15,6 +15,10 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
     timetable = json.loads(msg.payload)
+    timetable = [
+    {"id": "afK04drjIsDxu9I6dWjY","pet": "silver","owner": "aaaa@gmail.coooom","date": "02/02/2023, 09:16",
+     "schedule": "once","food": 100},{"id": "bG2ngtZQ6Xu6LsiMIy8U","pet": "silver","owner": "aaaa@gmail.coooom",
+    "date": "09/02/2023, 00:10","schedule": "everyday","food": 50}]
 
 
 client = mqtt.Client()
@@ -24,6 +28,8 @@ client.on_message = on_message
 client.connect("35.177.203.22", 1883, 60)
 print(datetime.datetime.timestamp)
 print(timetable)
+time = datetime.datetime.now()
+print(time)
 
 
 
