ab2ee197ee52f7555bcfe5f863df7ccb35569efc
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-02-16 19:35:00 +0000

    Update PetList.vue

--- src/Communication/webapp/frontend/src/components/PetList.vue
+++ src/Communication/webapp/frontend/src/components/PetList.vue
@@ -12,27 +12,40 @@ function deletePet(index) {
 
 <template>
   <div class="pet-list">
-    <div
-        v-for="(pet, index) in pets"
-        v-if="pets.length > 0"
-        :key="pet"
-        class="pet-item"
+    <v-card
+      v-for="(pet, index) in pets"
+      v-if="pets.length > 0"
+      :key="pet"
+      variant="tonal"
     >
-      <div class="pet-icon-container">
-        <font-awesome-icon :icon="['fa-solid', 'fa-dog']" class="dog-icon"/>
-        <font-awesome-icon :icon="['fa-solid', 'fa-cat']" class="dog-icon"/>
-      </div>
-      <div class="pet-container">
-        <div class="pet-desc">Name: {{ pet.name }}</div>
-        <div class="pet-desc">Weight: {{ pet.weight }} kg</div>
-      </div>
-      <div @click="deletePet(index)">
-        <font-awesome-icon
-            :icon="['fa-solid', 'fa-trash']"
-            class="trash-icon"
-        />
-      </div>
-    </div>
+      <v-card-title>
+        <div style="display: flex; flex-direction: column; justify-content: space-around">
+          <div class="pet-icon-container">
+            <font-awesome-icon
+              :icon="['fa-solid', 'fa-dog']"
+              class="dog-icon"
+            />
+            <font-awesome-icon
+              :icon="['fa-solid', 'fa-cat']"
+              class="dog-icon"
+            />
+          </div>
+          <v-card-actions>
+            <v-btn @click="deletePet(index)">Delete pet</v-btn>
+          </v-card-actions>
+        </div>
+        <div class="pet-container">
+          <div class="pet-desc" style='margin-bottom: 0.5em'>
+            <div class="pet-desc-header">Name</div>
+            <div>{{ pet.name }}</div>
+          </div>
+          <div class="pet-desc">
+            <div class="pet-desc-header">Weight</div>
+            <div>{{ pet.weight }} kg</div>
+          </div>
+        </div>
+      </v-card-title>
+    </v-card>
     <div v-else class="pet-list-empty">You haven't added any pets yet</div>
   </div>
 </template>
@@ -47,7 +60,18 @@ function deletePet(index) {
   justify-content: center;
 }
 
+:deep(.v-card) {
+  margin: 10px;
+  border-radius: 1em;
+}
+
+:deep(.v-card-title) {
+  padding: 0;
+  display: flex;
+}
+
 .pet-icon-container {
+  padding: 0.5em;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -58,40 +82,32 @@ function deletePet(index) {
   padding-left: 20px;
 }
 
-.pet-item {
-  border-style: solid;
-  border-radius: 10px;
-  font-size: 40px;
-  width: 600px;
-  height: 130px;
-  display: flex;
-  flex-direction: row;
-  margin: 20px;
-}
-
 .pet-desc {
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+}
+
+.pet-desc-header {
+  font-size: 20px;
+  font-weight: bold;
 }
 
 .pet-container {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
+  background-color: #ddd;
+  border: none;
+  color: black;
+  padding: 10px 30px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin-left: 1em;
+  border-radius: 16px;
 }
 
 .pet-list-empty {
   font-size: 30px;
-}
-
-.trash-icon {
-  font-size: 30px;
-  padding: 5px;
-}
-
-.trash-icon:hover {
-  cursor: pointer;
 }
 </style>
