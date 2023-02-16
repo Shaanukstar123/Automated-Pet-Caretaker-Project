32bdbf9dcf1d2528a48d0443f1ef6d2344bc53ef
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-02-16 19:33:36 +0000

    Update FeedingForm.vue

--- src/Communication/webapp/frontend/src/components/FeedingForm.vue
+++ src/Communication/webapp/frontend/src/components/FeedingForm.vue
@@ -1,131 +1,114 @@
 <script setup>
-import { useStore } from 'vuex';
-import { computed, defineEmits, ref } from 'vue';
-import BaseButton from './BaseButton.vue';
+import {useStore} from 'vuex';
+import {computed, defineEmits, ref, toRaw} from 'vue';
+import {Timestamp} from 'firebase/firestore';
 
 const store = useStore();
 const pets = computed(() => store.getters.petNames);
 const date = ref();
-const petName = ref(pets.value[0]);
-const food = ref(0);
+const petName = ref();
+const food = ref();
 const emit = defineEmits(['goto-feedhistory']);
-const feedingSchedule = ref('once');
+const feedingSchedule = ref();
+const feedings = computed(() => store.getters.feedings);
+const time = [];
+
+function convertDateToStamp(feeding) {
+  return new Timestamp(feeding.date.seconds, feeding.date.nanoseconds)
+      .toDate()
+      .getTime();
+}
 
 function submitFood() {
+  let feedingArray = toRaw(this.feedings);
+  let feedingForSubmitPet = feedingArray.filter(
+      (feeding) => feeding.pet === this.petName
+  );
+  if (feedingForSubmitPet.length > 0) {
+    const hasClosedFeedings = feedingForSubmitPet.filter(
+        (feeding) =>
+            Math.abs(convertDateToStamp(feeding) - this.date.getTime()) / 1000 < 120
+    )
+    if (hasClosedFeedings.length) {
+      window.alert('Feeding time is too close to another one. Select a different time')
+      return;
+    }
+  }
   store.dispatch('addFeeding', {
-    petName, food, date, feedingSchedule,
+    petName,
+    food,
+    date,
+    feedingSchedule,
   });
   emit('goto-feedhistory');
 }
 </script>
 
 <template>
-  <div class='feeding-form'>
-    <div class='feeding-from-wrapper'>
-      <div class='icons'>
-        <font-awesome-icon :icon="['fa-solid', 'fa-dog']" class='dog-icon' />
-        <font-awesome-icon
-          :icon="['fa-solid', 'fa-cat']" class='cat-icon'></font-awesome-icon>
-      </div>
-      <form class='form-card' @submit.prevent='submitFood'>
-        <div class='field'>
-          <label for='name'>Select your pet:</label>
-          <select id='name' v-model='petName' required>
-            <option v-for='pet in pets' :key='pet'>{{ pet }}</option>
-          </select>
-        </div>
-        <div class='field'>
-          <label for='food'>Enter food gram:</label>
-          <input v-model='food' class='input' required type='number'>
-        </div>
-        <div class='field datepicker-field'>
-          <label for='date'>Enter date and time:</label>
-          <div style='padding-left: 10px'>
-            <Datepicker v-model='date' :min-date='new Date()' class='datepicker-container' />
-          </div>
-        </div>
-        <div class='field datepicker-field'>
-          <label for='feeding-schedule'>Select feeding schedule:</label>
-          <select id='feeding-schedule' v-model='feedingSchedule' required>
-            <option value='once'>once</option>
-            <option value='everyday'>every day</option>
-            <option value='every week'>every week</option>
-          </select>
-        </div>
-        <div class='field button_wrapper'>
-          <BaseButton text='Add feeding' />
-        </div>
-      </form>
+  <div class="form-wrapper">
+    <div class="icons">
+      <font-awesome-icon :icon="['fa-solid', 'fa-dog']" class="dog-icon"/>
+      <font-awesome-icon :icon="['fa-solid', 'fa-cat']"/>
+    </div>
+    <div>
+      <v-sheet width="500">
+        <v-from @submit.prevent="submitFood">
+          <v-select
+              v-model="petName"
+              :items="pets"
+              aria-required="true"
+              clearable
+              density="compact"
+              label="Select your pet"
+          >
+          </v-select>
+          <v-text-field
+              v-model="food"
+              aria-required="true"
+              label="Pet's food in grams"
+              min="0"
+              single-line
+              type="number"
+          />
+          <label for="date">Enter date and time:</label>
+          <Datepicker
+              v-model="date"
+              :min-date="new Date()"
+              now-button-label="Current"
+          />
+          <v-select
+              v-model="feedingSchedule"
+              :items="['once', 'every day', 'every week']"
+              aria-required="true"
+              clearable
+              density="compact"
+              label="Select feeding schedule"
+              style="padding-top: 20px"
+          ></v-select>
+          <v-btn block type="submit" @click="submitFood()">Submit</v-btn>
+        </v-from>
+      </v-sheet>
     </div>
   </div>
 </template>
 
 <style scoped>
-.feeding-form {
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.feeding-from-wrapper {
-  padding: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
+  opacity: 1;
 }
 
 .icons {
-  display: flex;
+  font-size: xxx-large;
+  margin-bottom: 20px;
 }
 
 .dog-icon {
-  margin-bottom: 90px;
-  font-size: 100px;
-}
-
-.cat-icon {
-  padding-left: 15px;
-  margin-bottom: 90px;
-  font-size: 100px;
-}
-
-input, select {
-  font-size: 30px;
-  max-width: 300px;
-  margin-left: 30px;
-  border-radius: 5px;
-  padding-left: 20px;
-}
-
-select {
-  width: 300px;
-}
-
-.field {
-  font-size: 40px;
-  margin-bottom: 70px;
-}
-
-.datepicker-field {
-  display: flex;
-  align-items: center;
-}
-
-.datepicker-container {
-  width: 250px;
-}
-
-.button_wrapper {
-  display: flex;
-  justify-content: center;
-}
-
-button {
-  font-size: 40px;
-  padding: 10px;
-  border-radius: 10px;
+  padding-right: 10px;
 }
 </style>
