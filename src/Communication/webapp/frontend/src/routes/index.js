86f69e1e683dc84d61a280485ea7d16daf355cf5
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-02-16 19:35:41 +0000

    Update index.js

--- src/Communication/webapp/frontend/src/routes/index.js
+++ src/Communication/webapp/frontend/src/routes/index.js
@@ -2,7 +2,56 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { getAuth } from 'firebase/auth';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import Dashboard from '../views/Dashboard.vue';import {createRouter, createWebHistory} from 'vue-router';
+import {getAuth} from 'firebase/auth';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
 import Dashboard from '../views/Dashboard.vue';
+import Home from '../views/Home.vue';
+import CommercialPage from "../views/CommercialPage.vue";
+
+const routes = [
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+    }, {
+        path: '/commercial',
+        name: 'CommercialPage',
+        component: CommercialPage,
+    },
+
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+        beforeEnter: (to, from, next) => {
+            if (!getAuth().currentUser) {
+                next({path: '/login'});
+            } else {
+                next();
+            }
+        },
+    },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
+
 import Home from '../views/Home.vue';
 
 const routes = [
