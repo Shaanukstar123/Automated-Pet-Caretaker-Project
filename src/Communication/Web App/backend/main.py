c4c7d7b8020a4e5cf6d2d2386101df5b9ce5d3df
valiagiannopoulou <123558311+valiagiannopoulou@users.noreply.github.com>
2023-02-16 20:42:49 +0000

    Update main.py

--- src/Communication/Web App/backend/main.py
+++ src/Communication/Web App/backend/main.py
@@ -1,12 +1,16 @@
 # This is importing the libraries that are needed for the code to run.
 import json
+import paho.mqtt.client as mqtt
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import ssl
 
 # Creating a new instance of the FastAPI class.
+client = mqtt.Client()
+client.tls_set(cert_reqs=ssl.CERT_NONE)
+client.connect("35.177.203.22", port=8883)
 app = FastAPI(debug=True)
-# This is a middleware that allows the server to receive requests from any origin.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -14,7 +18,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 # The Feeding class has a string id, a string pet, a string owner, a string date, a string schedule,
 # and an integer food
@@ -36,21 +39,25 @@ class Feeding(BaseModel):
 # :param feeding: Feeding - This is the request body. It's a Feeding object
 # :type feeding: Feeding
 # """
+
+# This is the endpoint which receives the data
+@app.post("/")
+# This is function that is going to handle the request received
 async def create_feeding(feeding: Feeding):
-    # Converting the Feeding object into a dictionary.
     feeding_dict = feeding.dict()
-
-    # Opening the file feedings.json in read mode.
+    # Append dictionary into file and format file
     with open('feedings.json', 'r') as f:
-
-        # Trying to load the file feedings.json.
         try:
             feedings = json.load(f)
-        # If the file feedings.json is empty, then the json.load() function will raise a
-        # JSONDecodeError. This code is handling that error by setting the feedings variable to an
-        # empty list.
         except json.decoder.JSONDecodeError:
             feedings = []
+    with open('feedings.json', 'w') as f:
+        feedings.append(feeding_dict)
+        f.write(json.dumps(feedings, indent=4))
+        #print(feedings)
+        filtered_data = [{k: v for k, v in item.items() if k in ["pet", "date", "schedule", "food"]} for item in feedings] 
+    client.publish("timetable", json.dumps(feeding_dict))
+    return {"message": f"Feeding added {feeding}"}
 
     # Opening the file feedings.json in write mode.
     with open('feedings.json', 'w') as f:
